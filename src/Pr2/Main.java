7bb0bdb578834c9ee5193618dab389f97d8f93f5
youtipie18 <youtipie@gmail.com>
2024-10-14 13:52:19 +0300

    Fix

--- src/Pr2/Main.java
+++ src/Pr2/Main.java
@@ -1,10 +1,7 @@
 package Pr2;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class Main {
@@ -13,43 +10,66 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(4);
 
-        List<Future<Void>> futureList = Collections.synchronizedList(new ArrayList<>());
+        List<Future<RetutnPair>> futureList = Collections.synchronizedList(new ArrayList<>());
 
-        Callable<Void> task1 = createFactorialTask(5);
-        Callable<Void> task2 = createFactorialTask(10);
-        Callable<Void> task3 = createFactorialTask(15);
 
-        futureList.add(executorService.submit(task1));
-        futureList.add(executorService.submit(task2));
-        futureList.add(executorService.submit(task3));
+        futureList.add(executorService.submit(createFactorialTask(5)));
+        futureList.add(executorService.submit(createFactorialTask(10)));
+        futureList.add(executorService.submit(createFactorialTask(15)));
 
-        for (Future<Void> future : futureList) {
+        for (Future<RetutnPair> future : futureList) {
             if (future.isCancelled()) {
                 System.out.println("Task was cancelled");
             } else {
-                future.get();
+                RetutnPair res = future.get();
+                System.out.println("Factorial of " + res.number + " is " + res.factorialResult);
             }
         }
-
+        System.out.println("\n\nМемоїзована таблиця факторіалів: ");
         factorialMap.forEach((key, value) -> System.out.println("Factorial of " + key + " is " + value));
 
         executorService.shutdown();
     }
 
-    public static Callable<Void> createFactorialTask(int number) {
+    static class RetutnPair {
+        int number;
+        BigInteger factorialResult;
+
+        public RetutnPair(int number, BigInteger factorialResult) {
+            this.number = number;
+            this.factorialResult = factorialResult;
+        }
+    }
+
+    public static Callable<RetutnPair> createFactorialTask(int number) {
         return () -> {
-            BigInteger factorial = calculateFactorial(number);
-            factorialMap.put(number, factorial);
+            BigInteger factorial = getOrComputeFactorial(number);
+            factorialMap.putIfAbsent(number, factorial);
             System.out.println("Thread finished: " + Thread.currentThread().getName());
-            return null;
+            return new RetutnPair(number, factorial);
         };
     }
 
-    public static BigInteger calculateFactorial(int number) {
-        BigInteger result = BigInteger.ONE;
-        for (int i = 2; i <= number; i++) {
+    public static BigInteger getOrComputeFactorial(int number) {
+        Optional<Map.Entry<Integer, BigInteger>> nearestEntry = factorialMap.entrySet().stream()
+                .filter(entry -> entry.getKey() <= number)
+                .max(Map.Entry.comparingByKey());
+        BigInteger result;
+        int start;
+
+        if (nearestEntry.isPresent()) {
+            start = nearestEntry.get().getKey();
+            result = nearestEntry.get().getValue();
+        } else {
+            start = 1;
+            result = BigInteger.ONE;
+        }
+
+        for (int i = start + 1; i <= number; i++) {
             result = result.multiply(BigInteger.valueOf(i));
+            factorialMap.putIfAbsent(i, result);
         }
         return result;
     }
+
 }
